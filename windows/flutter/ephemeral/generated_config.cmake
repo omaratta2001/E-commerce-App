67a0b60312183e32fb0c11b39b5125abe6786aae
omaratta2001 <54627516+omaratta2001@users.noreply.github.com>
2022-05-07 19:07:56 +0200

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\ecommerice" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\src\\flutter"
+  "PROJECT_DIR=E:\\ecommerice"
+  "FLUTTER_ROOT=E:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\ecommerice\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\ecommerice"
+  "FLUTTER_TARGET=E:\\ecommerice\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\ecommerice\\.dart_tool\\package_config.json"
+)
